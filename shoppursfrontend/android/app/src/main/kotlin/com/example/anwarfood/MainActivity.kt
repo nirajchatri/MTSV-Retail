a89a739ed14751b7426559589e4a419311b320e6
nirajchatri@gmail.com <nirajchatri@gmail.com>
2025-06-21 15:46:56 +0530

    sucess

--- shoppursfrontend/android/app/src/main/kotlin/com/example/anwarfood/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mtsvretail
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- shoppursfrontend/lib/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'http://192.168.29.96:3000';
-} 
